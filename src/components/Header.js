23ed1a576498dd732371487230fc1bfa7bffbbe0
sandeep srinivasa <sss@lambdacurry.com>
2020-03-18 19:47:01 +0530

    new logo files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/icon-85px.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
