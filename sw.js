2e9129b7ecc68080dbe1c8e62c023d316391fd75
sandeep srinivasa <sss@lambdacurry.com>
2020-04-25 14:14:47 +0530

    Updates

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ self.__precacheManifest = [
     "url": "webpack-runtime-ee4bfe6dcd13f31bebbf.js"
   },
   {
-    "url": "commons-57cf6154e5fa7fd279b6.js"
+    "url": "commons-a7dfc05de91b915d99f5.js"
   },
   {
     "url": "app-d9326598cbe782344f51.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5f0a0ce8743de57297d911972f2a22bf"
+    "revision": "b32bd92ff6907e477e1318f15f89715a"
   },
   {
     "url": "manifest.webmanifest",
